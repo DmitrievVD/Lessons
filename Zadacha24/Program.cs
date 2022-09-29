8f97a5c81fc57e8592381a1c521c596aa8bb98f3
Viktor <fokus11548@mail.ru>
2022-09-29 08:21:53 +0500

    Сделал задачу 24

--- Zadacha24/Program.cs
+++ Zadacha24/Program.cs
@@ -1,2 +1,13 @@
 ﻿// Напишите программу которая принимает число (А), и выдает сумму чисел от 1 до А
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = 0;
+
+for (int i = 0; i <= num; i++ )
+{
+    result = result + i;
+    // Console.WriteLine(result);
+}
+Console.WriteLine($"Сумма чисел до числа {num}: {result}");
