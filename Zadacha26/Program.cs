6b14c712a8c7a9eb96f097bcfa86517828c5bff1
Viktor <fokus11548@mail.ru>
2022-09-29 08:35:41 +0500

    Сделал задачу 26

--- Zadacha26/Program.cs
+++ Zadacha26/Program.cs
@@ -1,2 +1,13 @@
 ﻿// Напишите программу которая принимает на вход число и выдает количество цифр в числе.
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int score = 0;
+int i = num;
+while (i > 0)
+{
+    i = i / 10;
+    score++;
+    // Console.WriteLine($"{i} || {score}");
+}
+Console.WriteLine($"Цифр в числе {num}: {score}");
