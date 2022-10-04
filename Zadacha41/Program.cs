f579c2af6c8c5ba1f77fe5715a59ea179bcff611
Viktor <fokus11548@mail.ru>
2022-10-05 01:14:21 +0500

    Сделал домашку 41,43 задача

---
+++ Zadacha41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int [] arr = new int [5];
+
+int i = 0;
+int score = 0;
+
+while (i < arr.Length)
+{
+    Console.WriteLine($"Введите {i+1}е число: ");
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+    i++;
+}
+
+for (int j = 0; j < arr.Length; j++)
+{
+    if (arr[j] > 0) score += 1;
+}
+Console.WriteLine($"Количество положительных чисел: {score}");
---
+++ Zadacha43/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Уравнения: ");
+Console.WriteLine("y = k1 * x + b1,");
+Console.WriteLine("y = k2 * x + b2;");
+Console.Write("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine();
+
+// double ResultX()
+// {
+//     double result = (ResultX()-b1)/k1;
+//     return result;
+// }
+double x = (b1 - b2) / (k2 - k1);
+double y = k2 * x + b2;
+Console.Write($"({x}; {y})");
