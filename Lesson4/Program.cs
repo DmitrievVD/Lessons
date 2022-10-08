b5c818f6ebb5db06b6d929244311c6d1f57a073f
Viktor <fokus11548@mail.ru>
2022-10-08 09:07:40 +0500

    Добавил пустыте папки с нерешенными задачами

---
+++ Lesson4/Program.cs
@@ -0,0 +1,134 @@
+﻿// string [,] table = new string [2,5];
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         Console.WriteLine($"-{table[rows, columns]}-");
+//     }
+// }
+
+
+
+// void PrintArrey(int[,] matr)
+// {
+//     for (int rows = 0; rows < matr.GetLength(0); rows++)
+//     {
+//         for (int columns = 0; columns < matr.GetLength(1); columns++)
+//         {
+//             Console.Write($"{matr[rows, columns]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// void FillArrey(int[,] matr)
+// {
+//     {
+//     for (int rows = 0; rows < matr.GetLength(0); rows++)
+//     {
+//         for (int columns = 0; columns < matr.GetLength(1); columns++)
+//         {
+//             matr[rows, columns] = new Random().Next(1, 10); // [1, 10)
+//         }
+//     }
+// }
+// }
+
+// int[,] matrix = new int [3,4];
+
+// PrintArrey(matrix);
+// FillArrey(matrix);
+// Console.WriteLine();
+// PrintArrey(matrix);
+
+
+
+int[,] pic = new int [,]
+{
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    {0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 },
+    {0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+    {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+};
+
+void PrintImage(int[,] image)
+{
+    for (int rows = 0; rows < image.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < image.GetLength(1); columns++)
+        {
+            // Console.Write($"{image[rows, columns]} ");
+            if (image[rows, columns] == 0) Console.Write($" ");
+            else Console.Write($"+");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillImage (int row, int col)
+{
+    if (pic[row, col] == 0)
+    {
+        pic[row, col] = 1;
+        FillImage(row-1, col);
+        FillImage(row, col-1);
+        FillImage(row+1, col);
+        FillImage(row, col+1);
+    }
+}
+
+PrintImage(pic);
+FillImage(13, 13);
+PrintImage(pic);
+
+
+
+// int Factorial(int n)
+// {
+//     // 1! = 1
+//     // 0! = 1
+//     if (n == 1) return 1;
+//     else return n * Factorial(n-1);
+// }
+
+// Console.WriteLine(Factorial(3));
+
+
+
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n - 1) + f(n - 2)
+
+// int Fibonachi(int n)
+// {
+//     if ( n == 1 || n == 2 ) return 1;
+//     else return Fibonachi(n - 1) + Fibonachi(n - 2);
+// }
+
+// int i = 1;
+// while (i < 10)
+// {
+//     Console.WriteLine(Fibonachi(i));
+//     i++;
+// }
+
---
+++ Zadacha39/Program.cs
@@ -0,0 +1,35 @@
+﻿// **Задача 39:** Напишите программу, 
+// которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+
+Console.Write("Напишите размер массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int [] arr = new int [N];
+int help = 0;
+int m = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(0, 10);
+}
+for (int i = 0; i < arr.Length; i++)
+{
+    if (i == arr.Length - 1) Console.Write($"{arr[i]} ");
+    else Console.Write($"{arr[i]}, ");
+
+}
+for (int i = 0; i < arr.Length/2; i++)
+{
+    m = arr.Length- 1 -i;
+    help = arr[i];
+    arr[i] = arr[m];
+    arr[arr.Length- 1 -i] = help;
+}
+Console.WriteLine();
+for (int i = 0; i < arr.Length; i++)
+{
+    if (i == arr.Length - 1) Console.Write($"{arr[i]} ");
+    else Console.Write($"{arr[i]}, ");
+
+}
